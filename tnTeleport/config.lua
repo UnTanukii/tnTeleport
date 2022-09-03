abc34539f597e00018034756f7812ee391df0264
Tanukii <79363399+UnTanukii@users.noreply.github.com>
2022-09-03 17:41:33 +0200

    Update config.lua

--- tnTeleport/config.lua
+++ tnTeleport/config.lua
@@ -3,6 +3,7 @@ tnTeleport = {}
 tnTeleport.Positions = {
     {
         ['Job'] = "police", --Only People with police job can use this teleporter
+        ['Job2'] = false, 
         ['Enter'] = {
             ['Coords'] = { x = -75.117118835449, y = -824.54711914063, z = 321.29183959961, h = 90},
             ['Notification'] = '~INPUT_CONTEXT~ pour descendre', --https://docs.fivem.net/docs/game-references/controls/
@@ -23,7 +24,8 @@ tnTeleport.Positions = {
         },
     },
     {
-        ['Job'] = false, --Everybody can use this teleporter
+        ['Job'] = false, 
+        ['Job2'] = "vagos", --Only people with vagos job can use this point 
         ['Enter'] = {
             ['Coords'] = { x = 206.33645629883, y = -1851.9584960938, z = 27.479230880737, h = 142.43612670898 },
             ['Notification'] = '~INPUT_CONTEXT~ pour entrer',
